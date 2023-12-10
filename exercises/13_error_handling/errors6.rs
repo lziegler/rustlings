8024d04d1c32d9e9f43d9e7ee11cbdd42ad3430c
Lukas Ziegler <lziegler@oss-group.co.nz>
2023-12-10 19:43:34 +1300

    more error handling

--- exercises/13_error_handling/errors6.rs
+++ exercises/13_error_handling/errors6.rs
@@ -9,8 +9,6 @@
 // Execute `rustlings hint errors6` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::num::ParseIntError;
 
 // This is a custom error type that we will be using in `parse_pos_nonzero()`.
@@ -24,14 +22,29 @@ impl ParsePosNonzeroError {
     fn from_creation(err: CreationError) -> ParsePosNonzeroError {
         ParsePosNonzeroError::Creation(err)
     }
-    // TODO: add another error conversion function here.
-    // fn from_parseint...
+
+    // for explicit conversoins `map_error()` - not commented out below
+    fn from_parseint(err: ParseIntError) -> ParsePosNonzeroError {
+        ParsePosNonzeroError::ParseInt(err)
+    }
+}
+
+// implicit conversion for ParseIntError
+impl From<ParseIntError> for ParsePosNonzeroError {
+    fn from(err: ParseIntError) -> Self {
+        ParsePosNonzeroError::ParseInt(err)
+    }
 }
 
 fn parse_pos_nonzero(s: &str) -> Result<PositiveNonzeroInteger, ParsePosNonzeroError> {
-    // TODO: change this to return an appropriate error instead of panicking
-    // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
+    // unwrap() will panic when `parse()` returns an error.
+    //let x: i64 = s.parse().unwrap();
+
+    // map ParseIntError using mapper function
+    //let x: i64 = s.parse().map_err(ParsePosNonzeroError::from_parseint)?;
+
+    // using implicit conversion
+    let x: i64 = s.parse()?;
     PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation)
 }
 
